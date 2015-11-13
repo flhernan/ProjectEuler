bbb5daedeb77cf15308052f7f69492ff2d39ba11
Francisco Hernandez <flhernan@ucsd.edu>
2015-11-13 15:55:45 -0800

    Largest Prime Factor

--- P03.py
+++ P03.py
@@ -1,18 +1,13 @@
 '''
 The prime factors of 13195 are 5, 7, 13 and 29.
-
-What is the largest prime factor of the number 600851475143 ?
 '''
+number = 600851475143
+'''
+What is the largest prime factor of this number?
+'''
+from eulerFunctions import isPrime
 from math import sqrt
 
-N = 600851475143
-rootN = (int)(sqrt(N))
-isFactor = lambda x: N % x==0
-factors = list(filter( isFactor, [x for x in range(2,rootN)]))
-primes = factors.copy()
-for i in factors:
-  for j in range(2,i):
-    if(i%j == 0):
-      if(i in primes):
-        primes.remove(i)
-print(primes)
+root_of_number = (int)(sqrt(number))
+isFactor = lambda x: number%x==0
+print(max([x for x in range(2,root_of_number) if isFactor(x) if isPrime(x)]))
