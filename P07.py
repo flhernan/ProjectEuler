cf98d0a1efa0411929871a3e4e3f4dd697bfe698
Francisco Hernandez <flhernan@ucsd.edu>
2015-11-13 17:47:18 -0800

    10001st prime

--- P07.py
+++ P07.py
@@ -1,26 +1,20 @@
 '''
-By listing the first six prime numbers:
-2, 3, 5, 7, 11, and 13,
-we can see that the 6th prime is 13.
+Problem 7
+---------
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that:
+the 6th prime is 13.
 
 What is the 10 001st prime number?
 '''
-def getPrimesAt(index):
-  primes = [2, 3]
+from eulerFunctions import isPrime
+def getPrimeAtIndex(index):
+
+  primes = [2]
   x = 3
-  while(True):
-    isPrime = True
-
-    for y in primes:
-      if x%y == 0:
-        isPrime = False
-        break
-
-    if isPrime == True:
+  while(len(primes) < index):
+    if isPrime(x):
       primes.append(x)
-
-    if index == len(primes):
-      return primes
-
     x += 2
-print(max(getPrimesAt(10001)))
+  return primes[-1]
+
+print(getPrimeAtIndex(10001))
