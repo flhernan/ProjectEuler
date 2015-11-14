c67e2dd2b1c005bd436eb02e3e9013825e4666cb
Francisco Hernandez <flhernan@ucsd.edu>
2015-11-13 22:30:51 -0800

    Summation of primes

--- P10.py
+++ P10.py
@@ -1,29 +1,21 @@
 '''
 Problem 10
-
+----------
 The sum of the primes below 10 is
 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes
 below two million.
 '''
-def sumPrimes(maxnum):
+from eulerFunctions import isPrime
+
+def sumPrimes(maxnum = 2000000):
+
   primes = [2]
-  k = 1
-  oner = 20001
   for i in range(3,maxnum,2):
-    if i % oner == 0:
-      print ("%s percent" % k)
-      k += 1
-      oner += 20000
-    isPrime = True
-    for j in primes:
-      if i % j == 0:
-        isPrime = False
-        break
-    if isPrime == True:
+    if isPrime(i):
       primes.append(i)
 
-  return(sum(primes))
+  return sum(primes)
 
-print(sumPrimes(2000000))
+print(sumPrimes())
