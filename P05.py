83d1e9d3bb08676dc32f94e69ab442f1c597b325
Francisco Hernandez <flhernan@ucsd.edu>
2015-11-13 16:54:17 -0800

    smallest multiple

--- P05.py
+++ P05.py
@@ -1,15 +1,10 @@
 '''
 Problem 5
+---------
+2520 is the smallest number that can be divided by each of the numbers from 1
+to 10 without any remainder.
 
-2520 is the smallest number that can be
-divided by each of the numbers from 1 to
-10 without any remainder.
-
-What is the smallest positive number that
-is evenly divisible by all of the numbers
-from 1 to 20?
-
-2^4*3^2*5*7*11*13*17*19
-'''''''''''''''''''''''''''''''''''''''''
-num = 2**4 * 3**2 * 5 * 7 * 11 * 13 * 17 * 19
-print(num)
+What is the smallest positive number that is evenly divisible by all of the
+numbers from 1 to 20?
+'''
+print(2**4 * 3**2 * 5 * 7 * 11 * 13 * 17 * 19)
